de5f84f0ecf5c13bef25dd74b4b4821f29f8362d
lihsus <sushilchhetri062@gmail.com>
2016-08-26 13:26:14 +0545

    Add files via upload

--- index.php
+++ index.php
@@ -1,24 +1,22 @@
+<html>
+<body bgcolor="#B2BABB">
+<center><h1>Simple XSS </h1></center>
+<center>
+<b>Dude name</b>
+<form action="name.php" method="POST">
+<input type="text" name="yourname"/><br/>
+<input type="submit" value="submit"/>
 
 
+</form>
+</center>
+<center>
+<br>
+<br>
 
-     <html>
-     <head></head>
-     <body>
-
-<?php
-	$name = $_POST['name'];
-	print "<center>$name";
-
-?>
-
-
-      <br>
-	<br>
-       <br>
-
-
-<button onclick="history.go(-1);">Back </button>
-
-
-       </body>
-       </html> 
+<label><b>Hint</b></label>
+<p>
+<b>This is simple form for injecting  cross site scripting. Try injecting javascript script tag to simply pop up the window. </b>
+</p>
+</body>
+</html>
---
+++ name.php
@@ -0,0 +1,12 @@
+<?php
+
+$name = $_POST['yourname'];  //getting data from index.php 
+echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<center><b>Welcome dude:&nbsp;".$name."<br/>how are you"; //make output of data sent by adding javascript code
+?>
+
+<br>
+<br>
+<br>
+
+<button onclick="history.go(-1);">Back </button>
+
