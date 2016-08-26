029e97e96381bcf2ff6276accd0c974514aaecb2
lihsus <sushilchhetri062@gmail.com>
2016-08-26 13:25:33 +0545

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+
+
+
+     <html>
+     <head></head>
+     <body>
+
+<?php
+	$name = $_POST['name'];
+	print "<center>$name";
+
+?>
+
+
+      <br>
+	<br>
+       <br>
+
+
+<button onclick="history.go(-1);">Back </button>
+
+
+       </body>
+       </html> 
